8f345bfe57dd36d162a1caf7d20b531fcc727998
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2026-03-17 15:40:34 +0100

    updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,9 +13,15 @@ resource "aws_key_pair" "supplychain360_key" {
 resource "aws_instance" "supplychain360_ec2" {
   ami           = "ami-0c17cb8e234335014"
   instance_type = var.instance_type
+
   key_name      = aws_key_pair.supplychain360_key.key_name
+
   subnet_id = aws_subnet.public_subnet.id
+
+  associate_public_ip_address = true
+
   vpc_security_group_ids = [aws_security_group.supplychain360_sg.id]
+  
   tags = {
     Name = "supplyChain360-instance"
   }
